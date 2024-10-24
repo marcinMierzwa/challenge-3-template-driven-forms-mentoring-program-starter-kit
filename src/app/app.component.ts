0d33630b4ed6d3cb4401fdcf9004eb641aca99a9
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-10-24 18:16:53 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 export interface User {
-  email: string,
-  username: string,
-  password: string,
-  roles: Role[],
-  accesslevel: boolean[]
+  email: string;
+  username: string;
+  password: string;
+  roles: string[];
+  accesslevel: boolean[];
 }
 
-export enum Role {
-  User = 'user',
-  Moderator = 'moderator',
-  Admin = 'admin'
+export interface UserResponse {
+  id: string;
 }
 
 @Component({
@@ -22,21 +20,33 @@ export enum Role {
   standalone: true,
   imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-
   title = 'mentoring-program-starter-kit';
   private httpClient: HttpClient = inject(HttpClient);
+  rolesOptions = ['User', 'Moderator', 'Admin'];
+  accesslevelOptions = ['Read', 'Write', 'Full'];
   user: User = {
-    email: "",
-    username: "",
-    password: "",
-    roles: [Role.User],
-    accesslevel: [false, false, false]
+    email: '',
+    username: '',
+    password: '',
+    roles: [],
+    accesslevel: [false, false, false],
+  };
+
+  isAtLeastOneAccessLevelSelected(): boolean {
+    return this.user.accesslevel.some((level) => level);
   }
 
-  onSubmitted(): void {
-    console.log(this.user);
+  onSubmitted() {
+    this.httpClient
+      .post<UserResponse>('https://fakestoreapi.com/users', this.user)
+      .subscribe({
+        next: (res: UserResponse) => {
+          alert(`user with id ${res.id} succesfully registered`);
+        },
+        error: (err) => console.log(err),
+      });
   }
 }
